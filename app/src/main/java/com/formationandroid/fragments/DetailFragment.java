ff03e628e7bfab908876d167f09946dbb999d908
mregnauld <mregnauld@gmail.com>
2020-01-12 13:56:29 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/fragments/DetailFragment.java
+++ app/src/main/java/com/formationandroid/fragments/DetailFragment.java
@@ -1,12 +1,13 @@
 package com.formationandroid.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 
 public class DetailFragment extends Fragment
@@ -28,7 +29,7 @@ public class DetailFragment extends Fragment
 	{
 		// init :
 		super.onActivityCreated(savedInstanceState);
-		if (getView() != null && getContext() != null)
+		if (getView() != null && getContext() != null && getArguments() != null)
 		{
 			// vues :
 			TextView textViewMemo = getView().findViewById(R.id.detail_memo);
--- app/src/main/java/com/formationandroid/fragments/MainActivity.java
+++ app/src/main/java/com/formationandroid/fragments/MainActivity.java
@@ -2,15 +2,16 @@ package com.formationandroid.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity
 {
